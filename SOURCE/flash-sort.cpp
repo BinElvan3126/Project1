ec22b268308b047d5a27215d82b9797ad0be73eb
Nagahuy <nagahao123@gmail.com>
2025-04-07 19:55:14 +0700

    Fix flash sort

--- SOURCE/flash-sort.cpp
+++ SOURCE/flash-sort.cpp
@@ -1,126 +1,102 @@
 #include "head.h"
 
-long long flashSort(int arr[], int n) {
+long long flashSort(int a[], int n){
     long long count = 0;
-    
-    if (n <= 1) 
+    if(n <= 1) 
     {
         count++;
         return count;
     }
-    
-    int minVal = arr[0], maxIdx = 0;
-    for (int i = 1; i < n; i++) 
-    {
-        if (arr[i] < minVal) 
+    int min = a[0], maxi = 0;
+    for (int i = 1; i < n; i++){
+        if(a[i] < min) 
         {
-            minVal = arr[i];
+            min = a[i];
             count++;
         }
-        if (arr[i] > arr[maxIdx])
+        if(a[i] > a[maxi])
         {
-            maxIdx = i;
+            maxi = i;
             count++;
         }
     }
-    int maxVal = arr[maxIdx];
-
-    if (maxVal == minVal)
-    {
+    int max = a[maxi];
+    if(max == min){
         count++;
         return count;
     }
-
     int m = int(0.43 * n);
-    if (m < 1) m = 1;
-    if (m > 300) m = 300;
-
+    if(m < 1){
+        m = 1;
+    }
+    if(m > 300){
+        m = 300;
+    }
     int* L = new int[m + 1];
-    for (int i = 0; i <= m; i++) L[i] = 0;
-
-    int c = (int)(m - 1) / (maxVal - minVal);
-    for (int i = 0; i < n; i++) 
-    {
-        int k = int(c * (arr[i] - minVal));
-        if (k < 0) 
-        {
+    for(int i = 0; i <= m; i++){
+        L[i] = 0;
+    }
+    int c = (int)(m - 1)/(max - min);
+    for(int i = 0; i < n; i++) {
+        int k = int(c* (a[i]- min));
+        if(k < 0){
             k = 0;
             count++;
         }
-        if (k >= m) 
-        {
+        if(k >= m){
             k = m - 1;
             count++;
         }
         L[k]++;
     }
-
-    for (int i = 1; i < m; i++) 
-    {
-        L[i] += L[i - 1];
+    for(int i = 1;i < m;i++){
+        L[i]+= L[i - 1];
     }
-
-    swap(arr[0], arr[maxIdx]);
-    int moves = 0;
-    int j = 0;
-    int k = m - 1;
-
-    while (moves < n - 1) 
-    {
-        while (j > L[k] - 1) 
-        {
+    swap(a[0],a[maxi]);
+    int moves, j,k;
+    moves=0, j=0, k=m-1;
+    while(moves < n - 1){
+        while(j > L[k] - 1){
             j++;
-            k = int(c * (arr[j] - minVal));
-
-            if (k < 0) 
-            {
+            k = int(c * (a[j] - min));
+            if(k < 0){
                 k = 0;
                 count++;
             }
-            if (k >= m) 
-            {
+            if(k >= m){
                 k = m - 1;
                 count++;
             }
         }
-        
-        int flash = arr[j];
-        while (j != L[k]) 
-        {
-            k = int(c * (flash - minVal));
-  
-            if (k < 0) 
-            {
+        int flash = a[j];
+        while(j != L[k]){
+            k = int(c*(flash - min));
+            if(k < 0){
                 k = 0;
                 count++;
             }
-            if (k >= m) 
-            {
-                k = m - 1;
+            if(k >= m){
+                k =m - 1;
                 count++;
             }
-            int temp = arr[L[k] - 1];
-            arr[L[k] - 1] = flash;
+            int temp = a[L[k] - 1];
+            a[L[k] - 1] = flash;
             flash = temp;
             L[k]--;
             moves++;
         }
     }
-
-    delete[] L;
-
-    for (int i = 1; i < n; i++) 
-    {
-        int key = arr[i];
+    for(int i = 1; i < n; i++){
+        int key = a[i];
         int j = i - 1;
-
-        while (j >= 0 && arr[j] > key)
+        while(j >= 0 && a[j] > key)
         {
-            arr[j + 1] = arr[j];
+            a[j + 1] = a[j];
             j--;
             count++;
         }
-        arr[j + 1] = key;
+        a[j + 1] = key;
     }
+    delete[] L;
     return count;
 }
